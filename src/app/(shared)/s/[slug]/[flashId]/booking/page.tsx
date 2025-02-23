ed3db9da57d4beb2e626866702fd234c315a6434
Diarmuid O' Donovan <63369803+diarm0d@users.noreply.github.com>
2025-02-23 12:51:06 +0300

    chore: css improvements

--- src/app/(shared)/s/[slug]/[flashId]/booking/page.tsx
+++ src/app/(shared)/s/[slug]/[flashId]/booking/page.tsx
@@ -45,8 +45,8 @@ export default async function BookingPage({
             <Card className="max-w-[800px] w-full mx-auto">
               <CardContent className="p-6  min-h-[450px] grid md:grid-cols-[1fr,auto,1fr] gap-4">
                 <div>
-                  <div className="flex items-center justify-center">
-                    <div className="w-lg sm:mw-xl">
+                  <div className="flex items-center">
+                    <div className="w-lg sm:mw-xl hidden sm:block">
                       <Image
                         alt={flash.flash?.title ?? ""}
                         className="aspect-square object-cover border border-gray-200 w-full rounded-lg overflow-hidden dark:border-gray-800"
@@ -55,44 +55,34 @@ export default async function BookingPage({
                         width={150}
                       />
                     </div>
-                  </div>
-                  <Avatar className="h-24 w-24">
-                    <AvatarImage
-                      alt={flash.flash?.profile?.name}
-                      src={flash.flash?.user?.image ?? undefined}
-                    />
-                    <AvatarFallback>
-                      {flash.flash?.profile?.name}
-                    </AvatarFallback>
-                  </Avatar>
-                  <p className="text-sm font-medium text-muted-foreground mt-1">
-                    {flash.flash?.profile?.name}
-                  </p>
-                  <h1 className="text-xl font-semibold mt-2">
-                    Tattoo of {flash.flash?.title}
-                  </h1>
-                  <p className="text-sm font-medium text-muted-foreground mt-1">
-                    {flash.flash?.description}
-                  </p>
-                  <div className="mt-5 flex flex-col gap-y-3">
-                    <p className="flex items-center">
-                      <CalendarX2 className="size-4 mr-2 text-primary" />
-                      <span className="text-sm font-medium text-muted-foreground">
-                        January 1, 2025
-                      </span>
-                    </p>
-                    <p className="flex items-center">
-                      <ClockIcon className="size-4 mr-2 text-primary" />
-                      <span className="text-sm font-medium text-muted-foreground">
-                        60 Minutes
-                      </span>
-                    </p>
-                    <p className="flex items-center">
-                      <MapPin className="size-4 mr-2 text-primary" />
-                      <span className="text-sm font-medium text-muted-foreground">
-                        Berlin
-                      </span>
-                    </p>
+                    <div className="ml-8">
+                      <h1 className="text-xl font-semibold mt-2">
+                        Tattoo of {flash.flash?.title}
+                      </h1>
+                      <p className="text-sm font-medium text-muted-foreground mt-1">
+                        By {flash.flash?.profile?.name}
+                      </p>
+                      <div className="mt-5 flex flex-col gap-y-3">
+                        <p className="flex items-center">
+                          <CalendarX2 className="size-4 mr-2 text-primary" />
+                          <span className="text-sm font-medium text-muted-foreground">
+                            {formattedDate}
+                          </span>
+                        </p>
+                        <p className="flex items-center">
+                          <ClockIcon className="size-4 mr-2 text-primary" />
+                          <span className="text-sm font-medium text-muted-foreground">
+                            {flash.flash?.profile?.sessionDuration} Minutes
+                          </span>
+                        </p>
+                        <p className="flex items-center">
+                          <MapPin className="size-4 mr-2 text-primary" />
+                          <span className="text-sm font-medium text-muted-foreground">
+                            Berlin
+                          </span>
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <Separator orientation="vertical" className="h-full w-[1px]" />
@@ -133,7 +123,7 @@ export default async function BookingPage({
               <CardContent className="p-10 min-h-[450px] grid grid-cols-1">
                 <div>
                   <div className="flex items-center">
-                    <div className="w-lg sm:mw-xl">
+                    <div className="w-lg sm:mw-xl hidden sm:block">
                       <Image
                         alt={flash.flash?.title ?? ""}
                         className="aspect-square object-cover border border-gray-200 w-full rounded-lg overflow-hidden dark:border-gray-800"
