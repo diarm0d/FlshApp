a73e79e1b8116b082c5ff576d7449bc2e95da7ac
Diarmuid O' Donovan <63369803+diarm0d@users.noreply.github.com>
2025-03-11 08:25:06 +0100

    feat: create order

---
+++ src/app/api/paypal/create-order.ts
@@ -0,0 +1,63 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+  Client,
+  Environment,
+  LogLevel,
+  OrdersController,
+  CheckoutPaymentIntent,
+} from "@paypal/paypal-server-sdk";
+
+// PayPal Client Configuration
+const client = new Client({
+  clientCredentialsAuthCredentials: {
+    oAuthClientId: process.env.PAYPAL_CLIENT_ID!,
+    oAuthClientSecret: process.env.PAYPAL_SECRET!,
+  },
+  timeout: 0,
+  environment: Environment.Sandbox,
+  logging: {
+    logLevel: LogLevel.Info,
+    logRequest: {
+      logBody: true,
+    },
+    logResponse: {
+      logHeaders: true,
+    },
+  },
+});
+
+const ordersController = new OrdersController(client);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { amount } = req.body;
+
+    try {
+      const collect = {
+        body: {
+          intent: CheckoutPaymentIntent.Capture,
+          purchaseUnits: [
+            {
+              amount: {
+                currencyCode: "EUR",
+                value: amount,
+              },
+            },
+          ],
+        },
+        prefer: "return=minimal",
+      };
+
+      const { result } = await ordersController.ordersCreate(collect);
+      res.json({ orderID: result.id });
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
