4124e05d8b3bce6350ef88d39e234125f279e2ba
Diarmuid O' Donovan <63369803+diarm0d@users.noreply.github.com>
2025-03-22 11:23:34 +0000

    feat: api auth fix

--- src/app/api/autocomplete/route.ts
+++ src/app/api/autocomplete/route.ts
@@ -10,18 +10,25 @@ export async function GET(req: NextRequest) {
   }
 
   try {
-    const response = await axios.get(
-      "https://maps.googleapis.com/maps/api/place/autocomplete/json",
+    const response = await axios.post(
+      "https://places.googleapis.com/v1/places:searchText",
       {
-        params: {
-          key: process.env.GOOGLE_MAPS_API_KEY,
-          input: input,
+        textQuery: input,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Goog-Api-Key": process.env.GOOGLE_MAPS_API_KEY,
+          "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.location",
         },
       }
     );
 
     return NextResponse.json(response.data);
   } catch (error) {
-    return NextResponse.json({ error: "Failed to fetch Google API data" }, { status: 500 });
+    return NextResponse.json(
+      { error: error || "Failed to fetch Google API data" },
+      { status: 500 }
+    );
   }
 }
